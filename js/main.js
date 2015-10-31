4ccfd874bf23e2cc9c8b34bb4e87e2735a4d1796
berwin <berwin7996@gmail.com>
2015-10-31 12:00:00 -0500

    style changes and adding mission statement

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // $("#about").hide();
+    $("#mission").hide();
+
+    $("#aboutbutton").click(function(){
+        // alert('hi');
+        $("#mission").fadeOut();
+        $("#about").fadeIn();
+    })
+
+    $("#missionbutton").hover(function(){
+        // $("#about").fadeOut();
+        $("#mission").fadeIn();
+    }, function(){
+        $("#mission").fadeOut();
+    });
+
+});
+
+
