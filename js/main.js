f4831c02988b7dc2a863cc77d1113365dcb6df8e
berwin <berwin7996@gmail.com>
2015-11-07 17:25:38 -0600

    collapsing faq and reversing the forms

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-    // $("#about").hide();
-    // $("#mission").hide();
+    $("#faq_a1").hide();
+    $("#faq_a2").hide();
+    $("#faq_a3").hide();
+    $("#faq_a4").hide();
 
     // $("#aboutbutton").click(function(){
     //     // alert('hi');
@@ -14,7 +16,29 @@ $(document).ready(function(){
     // }, function(){
     //     $("#mission").fadeOut();
     // });
+    $( "#faq_q1" ).click(function() {
+      $( "#faq_a1" ).slideToggle( "slow", function() {
+        // Animation complete.
+      });
+    });
 
+    $( "#faq_q2" ).click(function() {
+      $( "#faq_a2" ).slideToggle( "slow", function() {
+        // Animation complete.
+      });
+    });
+
+    $( "#faq_q3" ).click(function() {
+      $( "#faq_a3" ).slideToggle( "slow", function() {
+        // Animation complete.
+      });
+    });
+
+    $( "#faq_q4" ).click(function() {
+      $( "#faq_a4" ).slideToggle( "slow", function() {
+        // Animation complete.
+      });
+    });
 });
 
 
